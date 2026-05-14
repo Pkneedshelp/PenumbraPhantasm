6fef00aebc43aad66fd5b917a65888cda528ceae
SGUDestiny <panormov2006@gmail.com>
2026-05-15 01:38:05 +0400

    Removed debug great door spawner message

--- src/main/java/destiny/penumbra_phantasm/server/block/GreatDoorSpawnerBlock.java
+++ src/main/java/destiny/penumbra_phantasm/server/block/GreatDoorSpawnerBlock.java
@@ -1,18 +1,11 @@
 package destiny.penumbra_phantasm.server.block;
 
-import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.context.BlockPlaceContext;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
-import net.minecraft.world.phys.BlockHitResult;
 
 import javax.annotation.Nullable;
 
@@ -20,17 +13,6 @@ public class GreatDoorSpawnerBlock extends HorizontalDirectionalBlock {
     public GreatDoorSpawnerBlock(Properties pProperties) {
         super(pProperties);
         this.registerDefaultState(this.defaultBlockState().setValue(FACING, Direction.NORTH));
-    }
-
-    @Override
-    public InteractionResult use(BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer, InteractionHand pHand, BlockHitResult pHit) {
-        if (pLevel.isClientSide()) {
-            return InteractionResult.CONSUME;
-        }
-
-        pPlayer.displayClientMessage(Component.literal("Trying to do the thing"), true);
-
-        return InteractionResult.SUCCESS;
     }
 
     @Override
