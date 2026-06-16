00efd72ec46d2ed803b623d4dad268fe21de3d97
Pkneedshelp <estefanosocorro2026@gmail.com>
2026-06-16 14:05:42 -0500

    Refactor NBT methods and add ResourceLocation ID

--- src/main/java/destiny/penumbra_phantasm/capability/PlayerInventoryProvider.java
+++ src/main/java/destiny/penumbra_phantasm/capability/PlayerInventoryProvider.java
@@ -2,10 +2,11 @@ package destiny.penumbra_phantasm.capability;
 
 import net.minecraft.core.Direction;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.CapabilityManager;
-import net.minecraftforge.common.capabilities.ICapabilityProvider;
 import net.minecraftforge.common.capabilities.CapabilityToken;
+import net.minecraftforge.common.capabilities.ICapabilityProvider;
 import net.minecraftforge.common.util.LazyOptional;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -18,20 +19,22 @@ public class PlayerInventoryProvider implements ICapabilityProvider {
     private final PlayerInventoryData backend = new PlayerInventoryData();
     private final LazyOptional<PlayerInventoryData> optional = LazyOptional.of(() -> backend);
 
+    public static final ResourceLocation ID =
+            new ResourceLocation("penumbra_phantasm", "player_inventory");
+
     @NotNull
     @Override
     public <T> LazyOptional<T> getCapability(@NotNull Capability<T> cap, @Nullable Direction side) {
         return cap == PLAYER_INVENTORY ? optional.cast() : LazyOptional.empty();
     }
 
-    public CompoundTag saveNBT() {
+    public CompoundTag serializeNBT() {
         CompoundTag tag = new CompoundTag();
         backend.saveNBT(tag);
         return tag;
     }
 
-    public void loadNBT(CompoundTag tag) {
-        backend.loadNBT(tag);
+    public void deserializeNBT(CompoundTag nbt) {
+        backend.loadNBT(nbt);
     }
 }
-
