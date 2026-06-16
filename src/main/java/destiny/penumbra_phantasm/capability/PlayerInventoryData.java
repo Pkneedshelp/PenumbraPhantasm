0b81cb9cc9aeb0d7787f0fba6c2cde25f2454013
Pkneedshelp <estefanosocorro2026@gmail.com>
2026-06-16 12:15:36 -0500

    Add PlayerInventoryData class with NBT handling

--- src/main/java/destiny/penumbra_phantasm/capability/PlayerInventoryData.java
+++ src/main/java/destiny/penumbra_phantasm/capability/PlayerInventoryData.java
@@ -1,1 +1,52 @@
+package destiny.penumbra_phantasm.capability;
+
+import net.minecraft.nbt.CompoundTag;
+
+public class PlayerInventoryData {
+
+    private CompoundTag overworldInv = new CompoundTag();
+    private CompoundTag cardKingdomInv = new CompoundTag();
+    private String lastDimension = "";
+
+    public CompoundTag getOverworldInv() {
+        return overworldInv;
+    }
+
+    public CompoundTag getCardKingdomInv() {
+        return cardKingdomInv;
+    }
+
+    public void setOverworldInv(CompoundTag tag) {
+        this.overworldInv = tag;
+    }
+
+    public void setCardKingdomInv(CompoundTag tag) {
+        this.cardKingdomInv = tag;
+    }
+
+    public String getLastDimension() {
+        return lastDimension;
+    }
+
+    public void setLastDimension(String dim) {
+        this.lastDimension = dim;
+    }
+
+    public void saveNBT(CompoundTag nbt) {
+        nbt.put("overworld_inventory", overworldInv);
+        nbt.put("card_kingdom_inventory", cardKingdomInv);
+        nbt.putString("last_dimension", lastDimension);
+    }
+
+    public void loadNBT(CompoundTag nbt) {
+        if (nbt.contains("overworld_inventory"))
+            overworldInv = nbt.getCompound("overworld_inventory");
+
+        if (nbt.contains("card_kingdom_inventory"))
+            cardKingdomInv = nbt.getCompound("card_kingdom_inventory");
+
+        if (nbt.contains("last_dimension"))
+            lastDimension = nbt.getString("last_dimension");
+    }
+}
 
